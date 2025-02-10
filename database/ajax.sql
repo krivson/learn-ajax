86aea958df98d6ce2ab7cf637a8d40462d1e6c54
krivson <farelaranta.inbox@gmail.com>
2025-02-10 10:18:57 +0700

    Update database

--- database/ajax.sql
+++ database/ajax.sql
@@ -16,23 +16,30 @@
 
 
 -- Dumping database structure for ajax
+DROP DATABASE IF EXISTS `ajax`;
 CREATE DATABASE IF NOT EXISTS `ajax` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `ajax`;
 
 -- Dumping structure for table ajax.students
+DROP TABLE IF EXISTS `students`;
 CREATE TABLE IF NOT EXISTS `students` (
-  `id` int DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nis` varchar(50) DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
   `class` varchar(50) DEFAULT NULL,
   `major` varchar(50) DEFAULT NULL,
   `gender` varchar(50) DEFAULT NULL,
-  `address` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `address` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table ajax.students: ~0 rows (approximately)
+-- Dumping data for table ajax.students: ~2 rows (approximately)
+INSERT INTO `students` (`id`, `nis`, `name`, `class`, `major`, `gender`, `address`) VALUES
+	(1, '222310156', 'Farel Aranta', '12 Rekayasa Perangkat Lunak 2', 'Rekayasa Perangkat Lunak', 'Male', 'Jl. Imam Bonjol, No. 4 RT/RW 003/002'),
+	(3, '22231017', 'Frey Ayoga Azka', '12 Rekayasa Perangkat Lunak 2', 'Rekayasa Perangkat Lunak', 'Male', 'Jl. Imam Bonjol Prapatan');
 
 -- Dumping structure for table ajax.users
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
@@ -42,9 +49,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table ajax.users: ~0 rows (approximately)
+INSERT INTO `users` (`id`, `name`, `email`, `phone`, `city`, `created_at`, `updated_at`) VALUES
+	(7, 'Farel Aranta', 'farelaranta@gmail.com', '082110773354', 'Bekasi', '2025-02-10 01:56:28', '2025-02-10 01:56:51');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
